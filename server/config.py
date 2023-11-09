856185d50302d87b2688860eb8bb54245f23e0c9
Scott Schwab <scottschwab86@gmail.com>
2023-11-09 11:39:11 -0600

    Change database_uri back to live db

--- server/config.py
+++ server/config.py
@@ -28,8 +28,8 @@ bcrypt = Bcrypt(app)
 
 app.secret_key=os.environ.get('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
