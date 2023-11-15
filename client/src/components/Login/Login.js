b1e2b8269c47fb3cab8262dde4913dc49b0142c8
Scott Schwab <scottschwab86@gmail.com>
2023-11-15 14:59:23 -0600

    Set up redirect on user login success

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import styles from './Login.module.css'
+import styles from './Login.module.css';
+import { Redirect } from 'react-router-dom';
+
 
 
 function Login() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    fetch('/api/checkloginstatus')
+      .then(r => {
+        if (r.status === 200) {
+          setIsLoggedIn(true)
+        } else {
+          setIsLoggedIn(false)
+        }
+      })
+  }, [])
 
   const formSchema = yup.object().shape({
     username: yup.string().required("Must provide username"),
@@ -25,35 +40,43 @@ function Login() {
         },
         body: JSON.stringify(values)
       }).then((r) => {
-        console.log(r)
+        if (r.status === 200) {
+          setIsLoggedIn(true)
+        }
       })
     }
   })
 
+  // the redirect below works, but there's the flicker while it happens. Better solution?
   return (
-    <div className={styles.loginLayout}>
-      <form className={styles.loginForm} onSubmit={formik.handleSubmit}>
-        <h3>We're gonna need to see some id...</h3>
-        <small>Enter your username and password</small>
-        <input
-          placeholder="Username"
-          id="username"
-          name="username"
-          onChange={formik.handleChange}
-          value={formik.values.username}
-        ></input>
-        <input
-          placeholder="password"
-          id="password"
-          name="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        ></input>
+    <div>
+      {isLoggedIn ? <Redirect to='/' /> :
+        <div className={styles.loginLayout}>
+          <form className={styles.loginForm} onSubmit={formik.handleSubmit}>
+            <h3>We're gonna need to see some id...</h3>
+            <small>Enter your username and password</small>
+            <input
+              placeholder="Username"
+              id="username"
+              name="username"
+              onChange={formik.handleChange}
+              value={formik.values.username}
+            ></input>
+            <input
+              placeholder="password"
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            ></input>
 
-        <button type="submit">Login!</button>
+            <button type="submit">Login!</button>
 
-      </form>
+          </form>
+        </div>
+      }
     </div>
+
   )
 }
 
