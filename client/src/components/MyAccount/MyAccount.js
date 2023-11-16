0a78a054e0feabccaf436793ca90cb8410c093a2
Scott Schwab <scottschwab86@gmail.com>
2023-11-16 11:31:43 -0600

    Fix button styling issue

--- client/src/components/MyAccount/MyAccount.js
+++ client/src/components/MyAccount/MyAccount.js
@@ -52,11 +52,11 @@ function MyAccount() {
           <h2>Welcome back, {currentUser}!</h2>
 
           <p>Want to view, edit, or delete your own brew reviews?</p>
-          <Link to='/my-beers'><button>My Brew Reviews</button></Link>
+          <Link to='/my-beers'><button className={styles.accountBtn}>My Brew Reviews</button></Link>
           <p>Want to a review a new brew that <Link to='/beers'>no one else yas yet?</Link></p>
-          <Link to='/new'><button>New Beer</button></Link>
+          <Link to='/new'><button className={styles.accountBtn}>New Beer</button></Link>
           <p>Time to call it a day?</p>
-          <Link to='/logout'><button>Logout</button></Link>
+          <Link to='/logout'><button className={styles.accountBtn}>Logout</button></Link>
         </div>
 
         : <h1>Click <Link to='/login'>here</Link> to login!</h1>}
